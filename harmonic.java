bfb7bcb8524b89e3de823c9ff44a8c3f5d7a6a2a
alikrgnc <101356323+alikrgnc@users.noreply.github.com>
2022-09-01 13:23:00 +0300

    Add files via upload

---
+++ harmonic.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class harmonic {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayı : ");
+        int n = inp.nextInt();
+        double result = 0.0;
+        for (double i = 1; i<= n ; i++){
+            result += (1/i);
+
+        } System.out.print(result);
+    }
+}
