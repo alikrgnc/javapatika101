5df52ed0569c84bc11f271caa0f989b54c916575
alikrgnc <101356323+alikrgnc@users.noreply.github.com>
2022-09-01 12:53:40 +0300

    Add files via upload

---
+++ ussuSayi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ussuSayi {
+    public static void main(String[] args) {
+        int n, k;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı : ");
+        n = inp.nextInt();
+        System.out.print("Üs olacak sayı : ");
+        k = inp.nextInt();
+        int total = 1;
+
+        for (int i = 1; i<=k; i++){
+            total *= n;
+        }
+        System.out.println("cevap = " + total);
+    }
+}
